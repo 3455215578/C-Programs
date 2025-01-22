c4916fbfbbbd9a1ecc06f2b014dad35b87d74355
陈开烁 <3455215578@qq.com>
2025-01-23 02:44:29 +0800

    单链表完毕

--- 数据结构/7.3.单链表.c
+++ 数据结构/7.3.单链表.c
@@ -39,10 +39,10 @@ Node* Create_HeadNode(void)
 /** 遍历链表 **/
 /** 哑节点：添加在链表头部，不存储数据的节点 **/
 /** 在包含哑节点的链表中，"头节点"指的是这个哑节点本身，而"第一个节点"则是指哑节点之后的第一个实际数据节点。 **/
-void Travel_List(Node* list)
+void Travel_List(Node* head)
 {
 	// 让node指向node1
-	Node* node = list->next;
+	Node* node = head->next;
 
 	while (node != NULL)
 	{
@@ -92,23 +92,23 @@ void Travel_List(Node* list)
 
 /** 插入节点 -- 1.头插法 **/
 /** 每次都在头节点之后插入新节点 **/
-void Insert_Head(Node* list, ElemType element)
+void Insert_Head(Node* head, ElemType element)
 {
 	// 1.在堆内存中创建新节点，并给它的数据域赋值            
 	Node* new_node = (Node*)malloc(sizeof(Node));        
 	new_node->data = element; 
 
 	// 2.把头节点的指针域赋值给新节点
-	new_node->next = list->next;
+	new_node->next = head->next;
 
 	// 3.让头节点指向新节点
-	list->next = new_node;
+	head->next = new_node;
 }
 
 /** 获取尾节点 **/
-Node* Get_TailNode(Node* list)
+Node* Get_TailNode(Node* head)
 {
-	Node* node = list;
+	Node* node = head;
 
 	while (node->next != NULL)
 	{
@@ -120,11 +120,11 @@ Node* Get_TailNode(Node* list)
 
 /** 插入节点 -- 2.尾插法 **/
 /** 把新节点作为新的尾节点 **/
-void Insert_Tail(Node* list, ElemType element)
+void Insert_Tail(Node* head, ElemType element)
 {
 
 	// 每次都要获取链表的尾节点
-	Node* tail_node = Get_TailNode(list);
+	Node* tail_node = Get_TailNode(head);
 
 	// 1.在堆内存中创建新节点，并给它的数据域赋值            
 	Node* new_node = (Node*)malloc(sizeof(Node));
@@ -147,7 +147,7 @@ void Insert_Tail(Node* list, ElemType element)
 **/
 
 /** 寻找指定位置的前驱节点 **/
-Node* Find_PreNode(Node* list, int pos)
+Node* Find_PreNode(Node* head, int pos)
 {
 	if (pos == 0)
 	{
@@ -155,7 +155,7 @@ Node* Find_PreNode(Node* list, int pos)
 	}
 	else
 	{
-		Node* node = list;
+		Node* node = head;
 
 		for (int i = 0; i < pos - 1; i++) // pos=2
 		{
@@ -172,10 +172,10 @@ Node* Find_PreNode(Node* list, int pos)
 }
 
 /** 在指定位置插入节点 **/
-void Insert_Node(Node* list, int pos, ElemType element)
+void Insert_Node(Node* head, int pos, ElemType element)
 {
 	// 1.先找到插入位置的前驱节点
-	Node* pre_node = Find_PreNode(list, pos);
+	Node* pre_node = Find_PreNode(head, pos);
 
 	// 2.创建新节点
 	Node* new_node = (Node*)malloc(sizeof(Node));
@@ -190,10 +190,10 @@ void Insert_Node(Node* list, int pos, ElemType element)
 }
 
 /** 删除指定位置的节点并返回该节点存储的数据，同时释放该节点的内存空间 **/
-void Delete_Node(Node* list, int pos, ElemType* delete_data)
+void Delete_Node(Node* head, int pos, ElemType* delete_data)
 {
 	// 1. 先找到删除位置的前驱节点
-	Node* pre_node = Find_PreNode(list, pos);
+	Node* pre_node = Find_PreNode(head, pos);
 
 	if (pre_node->next == NULL)
 	{
@@ -213,9 +213,9 @@ void Delete_Node(Node* list, int pos, ElemType* delete_data)
 }
 
 /** 获取链表长度 **/
-int Get_Length(Node* list)
+int Get_Length(Node* head)
 {
-	Node* node = list;
+	Node* node = head;
 	int length = 0;
 
 	while (node != NULL)
