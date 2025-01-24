423eafc6f2f7b613b7c594f1e6cda79636e3bf72
陈开烁 <3455215578@qq.com>
2025-01-24 21:37:49 +0800

    上个版本的完善

--- 数据结构/7.4.3.单链表应用--删除中间节点.c
+++ 数据结构/7.4.3.单链表应用--删除中间节点.c
@@ -53,7 +53,6 @@ Node* Get_TailNode(Node* head)
 }
 
 /** 尾插法 **/
-
 void Insert_Tail(Node* head, ElemType element)
 {
 	Node* tail_node = Get_TailNode(head);
@@ -67,34 +66,43 @@ void Insert_Tail(Node* head, ElemType element)
 
 }
 
-// Head -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> NULL
-
-// pre           curr  
-// NULL    Head -> 1 ->  2 -> 3 -> 4 -> 5 -> 6 -> NULL
-
-/** 链表反转 **/
-void* Reverse_List(Node* head)
+/** 返回中间节点的前驱节点 **/
+Node* Find_MidPreNode(Node* head)
 {
-	Node* prev = NULL;
-	Node* curr = head->next;
-	Node* next; // 当curr指向prev时, next用来连接后面的节点
+	Node* fast = head->next;
+	Node* slow = head;
 
-	// 节点移动的顺序
-	while (curr != NULL)
+	while ((fast != NULL) && (fast->next != NULL))
 	{
-		// next保存当前节点的下一个节点，对于单链表，如果不保存，节点改变方向后就找不到旧节点的下一个节点了
-		next = curr->next;
-
-		curr->next = prev;
-
-		// 更新prev和curr的位置
-		prev = curr;
-		curr = next;
+		// 快指针一次走两步，慢指针一次走一步
+		fast = fast->next->next;
+		slow = slow->next;
 	}
 
-	// 让头节点指向prev
-	head->next = prev;
+	// 当fast为空或fast->next为空时，slow指向中间节点的前驱节点
+	return slow;
+}
 
+Node* Find_MidNode(Node* head)
+{
+	Node* pre_node = Find_MidPreNode(head);
+
+	return pre_node->next;
+}
+
+/** 删除中间节点-- 快慢指针 **/
+/** 对于偶数个节点，把中间靠后的节点定为中间节点 **/
+void Delete_MidNode(Node* head)
+{
+	Node* pre_node = Find_MidPreNode(head);
+
+	Node* mid_node = pre_node->next;
+
+	// 前驱节点直接指向中间节点的下一个节点
+	pre_node->next = mid_node->next;
+
+	// 释放中间节点的内存空间
+	free(mid_node);
 }
 
 int main(void)
@@ -110,7 +118,7 @@ int main(void)
 
 	Travel_List(list);
 
-	Reverse_List(list);
+	Delete_MidNode(list);
 
 	Travel_List(list);
 
