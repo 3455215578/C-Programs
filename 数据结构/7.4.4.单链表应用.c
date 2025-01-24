672ecc5857c8d012172e2b2a038c8904e80f893f
陈开烁 <3455215578@qq.com>
2025-01-25 01:56:05 +0800

    未实现

--- 数据结构/7.4.4.单链表应用.c
+++ 数据结构/7.4.4.单链表应用.c
@@ -69,6 +69,74 @@ void Insert_Tail(Node* head, ElemType element)
 
 }
 
+/** 寻找指定位置的前驱节点 **/
+Node* Find_PreNode(Node* head, int pos)
+{
+	if (pos == 0)
+	{
+		printf("头节点没有前驱节点\n");
+	}
+	else
+	{
+		Node* node = head;
+
+		for (int i = 0; i < pos - 1; i++)
+		{
+			if (node->next == NULL)
+			{
+				return;
+			}
+
+			node = node->next;
+		}
+
+		return node;
+	}
+}
+
+/** 在指定位置插入节点 **/
+void Insert_Node(Node* head, int pos, ElemType element)
+{
+	// 1.先找到插入位置的前驱节点
+	Node* pre_node = Find_PreNode(head, pos);
+
+	// 2.创建新节点
+	Node* new_node = (Node*)malloc(sizeof(Node));
+	new_node->data = element;
+
+	// 3.让新节点与前驱节点原来所指向的节点相连
+	new_node->next = pre_node->next;
+
+	// 4.让前驱节点与新节点相连
+	pre_node->next = new_node;
+
+}
+
+/** 链表反转 **/
+void* Reverse_List(Node* head)
+{
+	Node* prev = NULL;
+	Node* curr = head->next;
+	Node* next; // 当curr指向prev时, next用来连接后面的节点
+
+	// 节点移动的顺序
+	while (curr != NULL)
+	{
+		// next保存当前节点的下一个节点，对于单链表，如果不保存，节点改变方向后就找不到旧节点的下一个节点了
+		next = curr->next;
+
+		curr->next = prev;
+
+		// 更新prev和curr的位置
+		prev = curr;
+		curr = next;
+	}
+
+	// 让头节点指向prev
+	head->next = prev;
+
+}
+
 /** 返回中间节点的前驱节点 **/
 Node* Find_MidPreNode(Node* head)
 {
@@ -108,6 +176,14 @@ void Delete_MidNode(Node* head)
 	free(mid_node);
 }
 
+//Init: Head 1 2 3 4 5 6 NULL
+//End:  Head 1 6 2 5 3 4 NULL
+
+// Head 1 2 3 NULL
+// Head 4 5 6 NULL
+
+// Head 1 2 3 NULL
+// Head 6 5 4 NULL
 int main(void)
 {
 	Node* list = Create_HeadNode();
@@ -119,11 +195,41 @@ int main(void)
 	Insert_Tail(list, 5);
 	Insert_Tail(list, 6);
 
-	Travel_List(list);
+	// 1.将原链表中的中间节点及以后的节点移动到新链表
+	Node* new_list = Create_HeadNode();
 
-	Delete_MidNode(list);
+	Node* pre_node = Find_MidPreNode(list);
+	Node* temp1 = pre_node->next;
 
-	Travel_List(list);
+	while (temp1 != NULL)
+	{
+		Insert_Tail(new_list, temp1->data);
+		temp1 = temp1->next;
+	}
+
+	pre_node->next = NULL;
+
+	// 2.将新链表反转
+	Reverse_List(new_list);
+
+	// 3.将新链表的节点间隔插入到原链表
+
+	temp1 = list->next;
+	Node* temp2 = new_list->next;
+
+	// 保存原链表和新链表的节点
+	Node *temp_node1, *temp_node2;
+	while ((temp1 != NULL) && (temp2 != NULL))
+	{
+		temp_node1 = temp1;
+		temp_node2 = temp2;
+
+		temp2->next = temp_node1->next;
+		temp1->next = temp2;
+
+
+	}
+
 
 	return 0;
 }
